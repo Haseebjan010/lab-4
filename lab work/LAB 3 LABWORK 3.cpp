dbd247f5e804f40c6810aaff0549048799f9655e
Haseebjan010 <ha4051482@gmail.com>
2026-03-03 23:07:10 +0500

    Add files via upload

---
+++ lab work/LAB 3 LABWORK 3.cpp
@@ -0,0 +1,62 @@
+//
+//                       ___________________________________________________________________
+//                      | Name: Haseeb Ahmad                                                |
+//                      | Department: Electrical Engineering (Communication)                |
+//                      | Registration No: BF25NWELE0678                                    |
+//                      | Section: B                                                        |
+//                      |___________________________________________________________________|
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+    
+    	char grade=0;
+    	cout<<"enter your grade (A,B,C,D,E,F)"<<endl;
+    	cin>>grade;
+    	
+    	switch(grade)
+    {
+			case 'A':
+			case 'a':
+cout<<"excellent";
+break;
+
+	
+    	case'B':
+    		case 'b':
+cout<<"very good";
+break;
+
+	case 'C':
+		case 'c':
+cout<<"good";
+break;
+
+	case 'D':
+		case 'd':
+cout<<"Pass";
+break;
+
+	case 'E':
+		case 'e':
+cout<<"barely passed";
+break;
+
+	case 'F':
+		case 'f':
+cout<<"repeat again";
+break;
+
+
+
+default:
+	cout<<"enter correct grade";
+	
+}
+
+return 0;
+}
+
